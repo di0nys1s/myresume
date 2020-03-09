df4a50c94bfce0a768628365fafdc15a2fa3f3f4
Burak Seyhan <seyhanburak9@gmail.com>
2020-03-10 08:58:54 +1100

    title update

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -28,7 +28,7 @@ const Experience = () => {
 
         <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
           <div className="resume-content">
-            <h3 className="mb-0">Junior Frontend Developer</h3>
+            <h3 className="mb-0">Frontend Developer</h3>
             <div className="subheading mb-3">
               <a target="_blank" href="z">
                 AEFIS LLC
