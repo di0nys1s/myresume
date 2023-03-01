ce9eba1a8e76040e323e56c18932e0dc2342e563
Burak Seyhan <burak@netfront.com.au>
2023-03-01 10:45:23 +0100

    Link updates

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -6,21 +6,6 @@ const Experience = () => {
       className="resume-section p-3 p-lg-5 d-flex justify-content-center"
       id="experience"
     >
-      <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-        <div className="resume-content">
-          <h3 className="mb-0">Frontend Engineer</h3>
-          <div className="subheading mb-3">
-            <a target="_blank" href="https://www.igeniusgroup.com/">
-              RTL
-            </a>
-          </div>
-          <p></p>
-        </div>
-        <div className="resume-date text-md-right">
-          <span className="text-primary">January 2023 - Present</span>
-        </div>
-      </div>
-
       <div className="w-100">
         <h2 className="mb-5">Experience</h2>
 
@@ -28,7 +13,22 @@ const Experience = () => {
           <div className="resume-content">
             <h3 className="mb-0">Frontend Engineer</h3>
             <div className="subheading mb-3">
-              <a target="_blank" href="https://www.igeniusgroup.com/">
+              <a target="_blank" href="https://www.rtl.nl">
+                RTL
+              </a>
+            </div>
+            <p></p>
+          </div>
+          <div className="resume-date text-md-right">
+            <span className="text-primary">January 2023 - Present</span>
+          </div>
+        </div>
+
+        <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div className="resume-content">
+            <h3 className="mb-0">Frontend Engineer</h3>
+            <div className="subheading mb-3">
+              <a target="_blank" href="https://netfront.com.au">
                 Netfront
               </a>
             </div>
@@ -47,7 +47,7 @@ const Experience = () => {
           <div className="resume-content">
             <h3 className="mb-0">Full Stack Developer</h3>
             <div className="subheading mb-3">
-              <a target="_blank" href="https://www.igeniusgroup.com/">
+              <a target="_blank" href="https://www.igeniusgroup.com">
                 iGenius Group
               </a>
             </div>
@@ -69,7 +69,7 @@ const Experience = () => {
           <div className="resume-content">
             <h3 className="mb-0">Software Developer (Volunteer)</h3>
             <div className="subheading mb-3">
-              <a target="_blank" href="https://www.thecrag.com/">
+              <a target="_blank" href="https://www.thecrag.com">
                 theCrag.com
               </a>
             </div>
